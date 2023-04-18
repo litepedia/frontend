248448a157b7107f4bff8c141517dcc6acf27648
doron aviguy <doronaviguy@gmail.com>
2023-04-18 14:21:13 +0300

    search input

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -17,6 +17,7 @@ export const SearchInput = () => {
   const [showResults, setShowResults] = useState(false);
   const ref = useRef<any>();
 
+  
   useOnClickOutside(ref, () => setShowResults(false));
 
   const onSubmit = () => {
@@ -25,6 +26,7 @@ export const SearchInput = () => {
     }
   };
 
+  
   const search = (value: string) =>
     router.push(`/search/${value}`, {
       forceOptimisticNavigation: true,
