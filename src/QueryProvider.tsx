742ed311c3457a31c752fe02606475c2e19fb41f
doron aviguy <doronaviguy@gmail.com>
2023-04-19 20:55:03 +0300

    x3

--- src/QueryProvider.tsx
+++ src/QueryProvider.tsx
@@ -11,6 +11,9 @@ export default function QueryProvider({
   children: React.ReactNode;
 }) {
   
+
+
+  
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
